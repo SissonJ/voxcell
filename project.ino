78165a632207fdb776aee3b406aea3168073b710
SissonJ <sissonjack67@gmail.com>
2022-05-10 12:54:12 -0500

    hey

--- project.ino
+++ project.ino
@@ -5,6 +5,7 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
+// Test
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
